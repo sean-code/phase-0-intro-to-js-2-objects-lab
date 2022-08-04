0a157518caeb67f84403abfc72078fd1e72e7b2f
sean-code <john.nganga@student.moringaschool.com>
2022-08-04 22:56:37 +0300

    Completed assignment first four tests

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+// TEST ONE
+let employee = {
+    name: 'SamSpeedy',
+    streetAddress: 'Abuja Nigeria',
+}
+// TEST TWO
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+// TEST THREE
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+    
+}
+// TEST FOUR
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    delete employee[key];
+    return employee;
+};
